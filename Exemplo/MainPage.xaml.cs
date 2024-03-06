a638f06c3f2a5f74ffc2b223c2ee4f5c1a212000
EricaBidinoti <ericabidinoti@gmail.com>
2024-03-06 18:18:29 -0300

    feat: adicionei enviar mensagem

---
+++ Exemplo/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace Exemplo
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string name = " ";
+
+        //Metodo Construtor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+        }
+
+        private void OnMessageClicked(object sender, EventArgs e)
+        {
+            name = NameEntry.Text;
+            MessageLabel.Text =$"Hello, {name}!";
+        }
+    }
+
+}
